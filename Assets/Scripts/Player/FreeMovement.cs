23e6cdae2e1de69a4dac5b40649ec640b82b9082
Daniel Alfredo Rayo Roldán <ray22933@uvg.edu.gt>
2026-03-05 17:52:24 -0600

    feat: add free movement and new collider

--- Assets/Scripts/Player/FreeMovement.cs
+++ Assets/Scripts/Player/FreeMovement.cs
@@ -1,6 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
-public class PlayerMovement : MonoBehaviour
+public class FreeMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator    = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        animator.SetBool("isWalking", true);
+
+        if (ctx.canceled)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        moveInput = ctx.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }
+    
 }
