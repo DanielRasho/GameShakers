0fa64307c2b557dae9bccbbc47a1dd949f99fa22
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-03-06 09:15:10 -0600

    feat: add toggle informes, informecontroller and change levelManager

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] private GameObject victoryUI;
     private int coins = 0;
+
+    [SerializeField] private int informesRequired;
+    private int informes = 0;
+    public int Informes => informes;
+
+    [SerializeField] private Toggle informeToggle;
+
 
     // ---- COINS -----
     public int Coins
@@ -29,4 +37,17 @@ public class LevelManager : MonoBehaviour
             victoryUI.SetActive(true);
         }
     }
+
+    // --- Informe ---
+    public void AddInforme()
+    {
+        if (informes < informesRequired)
+            informes++;
+
+        Debug.Log("Informe recogido: " + informes);
+
+        if (informes >= informesRequired)
+            informeToggle.isOn = true;
+    }
+
 }
